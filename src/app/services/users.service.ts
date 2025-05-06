f0bb8914aaa61aa94517bc69491d1d8a2789d1bc
Amanda <amanda.heryprieto@etu.unilim.fr>
2025-05-06 16:32:14 +0200

    api

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class UsersService {
 
-  private apiUrl = 'http://localhost:3000/users';  // Backend
+  private apiUrl = 'https://l314s2-production.up.railway.app/users';  // Backend
 
   constructor(private http: HttpClient) {}
 
